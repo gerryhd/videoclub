9a4abc536ba1c302116f7c15ffc870acf5414681
Gerry <gerry_hd@live.com.mx>
2017-06-02 13:53:04 -0500

    added search for responsive index

--- app/assets/javascripts/search_movie.js
+++ app/assets/javascripts/search_movie.js
@@ -3,17 +3,19 @@ $(document).ready(function() {
         var query = $('#search_query').val();
         console.log("debug: ");
         var movies = $('.movie-panel');
+        var minis = $('.mini-movie');
         if (query) {
 
             for (i = 0; i < movies.length; i++) {
                 if (!movies[i].textContent.toLowerCase().includes(query)) {
                     movies[i].style.display = 'none';
+                    minis[i].style.display = 'none';
                 }
             }
         } else {
             for (i = 0; i < movies.length; i++) {
                 movies[i].style.display = 'inline-block';
-
+                minis[i].style.display = 'inline-block';
             }
         }
     });
